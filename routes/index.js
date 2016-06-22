5e402b47db23676514be128e9d7012869e9b44fa
Trevor Pham <trevor.dt.pham@gmail.com>
2016-06-22 09:38:25 -0700

    remove dead routes

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/characters/:apikey', main.getCharacterList);
-router.get('/items/:name', main.getItemList);
 router.get('/search/:apikey/:item', main.searchItem);
 
 module.exports = router;
